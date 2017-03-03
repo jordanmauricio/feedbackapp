1c42c4e719f20ebaaaccca55cf4471cbde950419
jordan.mauricio <jordan.mauricio@trimm.nl>
2017-03-03 16:29:26 +0100

    Presentation sliders working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.goToNext = this.goToNext.bind(this);
+
+    this.state = {
+      count: 0,
+    }
+  }
+
+  goToNext(){
+    
+    const newQuestions = document.querySelector( '#newQuestions' );
+    document.querySelector( '#nextButton' ).innerHTML = "Continue";
+
+    if(newQuestions.hasChildNodes()){
+      newQuestions.innerHTML = '';
+    }
+    
+    const questions = Array.from(document.querySelector( '#temp-questions' ).childNodes);
+    const question = questions.map((q) => q.innerHTML );
+
+    if( this.state.count < questions.length){
+        newQuestions.insertAdjacentHTML('afterbegin', question[this.state.count]);
+        this.setState({ count: this.state.count+1 });
+    } else {
+        newQuestions.insertAdjacentHTML('afterbegin', "<li><h1 className='blue'>I ran out of numbers...<h1></li>");
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="questions">
+        <ol id="temp-questions">
+          <li><h1>1</h1></li>
+          <li><h1>2</h1></li>
+          <li><h1>3</h1></li>
+        </ol>
+
+        <ul id="newQuestions"></ul>
+        <div id="nextButton" onClick={this.goToNext}>Start</div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from "react-router";
 import App from './App';
+import Home from './Home.jsx';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <Router history={browserHistory}>
+  	<Route path="/" component={App} />
+  	<Route path="/home" component={Home} />
+  </Router>
+  ), document.getElementById('root'))
