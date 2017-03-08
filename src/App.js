f546b855f3ee56e47bc6d2a62bec8310f3d38298
Jordan Mauricio <jordan.mauricio@trimm.nl>
2017-03-08 13:30:13 +0100

    Submit button working

--- src/App.js
+++ src/App.js
@@ -10,57 +10,96 @@ class App extends Component {
         this.handleSelections = this.handleSelections.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.generateQuestions = this.generateQuestions.bind(this);
+        // this.pushAnswers = this.pushAnswers.bind(this);
+        // this.sendData = this.sendData.bind(this);
+        this.callback = this.callback.bind(this);
+        // this.sleep = this.sleep.bind(this);
 
         this.data = {
-            "id": "projectId",
-            "title": "Client Name",
-            "description": "This will be our introduction text",
+            "id": "anotherProject",
+            "title": "Unicarriers",
+            "description": "As part of TRIMM's commitment to providing the best possible service to our clients, we kindly request you to fill out this short questionnaire regarding our performance in our last sprint.",
             "questions": [
                 {
-                    "name": "product",
-                    "question": "Are YOU happy with our product?",
+                    "name": "proactive",
+                    "question": "To which extent would you consider TRIMM to be a proactive partner to work with?",
                     "answers": [
                         {
-                            "answer": "Fanstastic!",
-                            "value": "fantastic",
+                            "answer": "Terrible",
+                            "value": "1",
                         },
                         {
-                            "answer": "Absolutely shit.",
-                            "value": "shit",
-                        }
-                        
-                        
-                    ]
-                },
-
-                {
-                    "name": "scrum",
-                    "question": "How did YOU find our Scrum process?",
-                    "answers": [
-                        {
-                            "answer": "Excellent!",
-                            "value": "excellent",
+                            "answer": "Bad",
+                            "value": "2",
                         },
                         {
-                            "answer": "Never again.",
-                            "value": "never",
+                            "answer": "Neutral",
+                            "value": "3",
+                        },
+                        {
+                            "answer": "Good",
+                            "value": "4",
+                        },
+                        {
+                            "answer": "Excellent",
+                            "value": "5",
                         },
                         
                     ]
                 },
 
                 {
-                    "name": "bridge",
-                    "question": "What's your opinion on the plausibility of the Einstein-Rosen bridge?",
+                    "name": "sprint",
+                    "question": "How satisfied are you with the results of the past sprint?",
                     "answers": [
                         {
-                            "answer": "Everyone forgets about Ludwig!",
-                            "value": "ludwig"
+                            "answer": "Terrible",
+                            "value": "1",
                         },
                         {
-                            "answer": "Does Schwarzschild even HAVE a first name?",
-                            "value": "schwarzschild"
-                        }
+                            "answer": "Bad",
+                            "value": "2",
+                        },
+                        {
+                            "answer": "Neutral",
+                            "value": "3",
+                        },
+                        {
+                            "answer": "Good",
+                            "value": "4",
+                        },
+                        {
+                            "answer": "Excellent",
+                            "value": "5",
+                        },
+                        
+                    ]
+                },
+
+                {
+                    "name": "budget",
+                    "question": "How satisfied are you with the provided budget updates?",
+                    "answers": [
+                        {
+                            "answer": "Terrible",
+                            "value": "1",
+                        },
+                        {
+                            "answer": "Bad",
+                            "value": "2",
+                        },
+                        {
+                            "answer": "Neutral",
+                            "value": "3",
+                        },
+                        {
+                            "answer": "Good",
+                            "value": "4",
+                        },
+                        {
+                            "answer": "Excellent",
+                            "value": "5",
+                        },
                     ]
                 }
             ]
@@ -68,14 +107,13 @@ class App extends Component {
 
         this.state = {
             questionCount: 1,
+            submitClass: "",
         };
 
-        this.selectedOptions = [
-            { "name": "product", "value":false },
-            { "name": "scrum", "value":false },
-            { "name": "bridge", "value":false }
-        ];
-
+        this.selectedOptions = [];
+        for(let question of this.data.questions){
+            this.selectedOptions.push({"name": question.name, "value": false});
+        } 
     }
 
     handleSelections(value, name){
@@ -97,18 +135,25 @@ class App extends Component {
     handleSubmit(){
         //send to Firebase
         console.log(this.selectedOptions);
-        // this.selectedOptions.forEach((answer) => {
-        //     var immediatelyAvailableReference = base.push(`${this.data.id}/answers`, {
-        //         data: answer,
-        //         then(err){
-        //             if(!err){
-        //                 console.log("Firebase sent.");
-        //             }
-        //         }
-        //     });
-        //     //available immediately, you don't have to wait for the callback to be called
-        //     immediatelyAvailableReference.key = answer.name;
-        // });
+
+        this.setState({ submitClass: "processing" });
+
+        this.selectedOptions.map((answer) => {
+            var immediatelyAvailableReference = base.push(`${this.data.id}/answers`, {
+                data: answer,
+                then: this.callback
+            });
+            //available immediately, you don't have to wait for the callback to be called
+            var generatedKey = immediatelyAvailableReference.key;
+        });
+    }
+
+    callback(err) {
+        if(!err){
+            //console.log("callback fn");
+            this.setState({ submitClass: "doneProcessing" });
+            document.querySelector( "#submitButton h4" ).textContent = "Done";
+        }
     }
 
     generateQuestions(){
@@ -131,11 +176,11 @@ class App extends Component {
         <div className="questionsList">
             <div id="block-counter"></div>
 
-            <div id="introduction-text"><img src="/TRIMM.svg"/><h1>{this.data.title}</h1><br/><h3>{this.data.description}</h3></div>
+            <div id="introduction-text"><img src="/TRIMM.svg" alt="TRIMM logo"/><h1>{this.data.title}</h1><br/><h3>{this.data.description}</h3></div>
 
             <ul id="currentQuestion">{this.generateQuestions()}</ul>
 
-            <div id="submitButton" onClick={this.handleSubmit}><h1>Submit</h1></div>
+            <div id="submitButton" onClick={this.handleSubmit}><h4 className={`defaultButton ${this.state.submitClass}`}>Submit</h4></div>
         </div>
         );
     }
