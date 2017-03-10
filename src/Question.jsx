da5038d2dcd6198eb046d6a330a921f6e158c5e1
Jordan Mauricio <jordan.mauricio@trimm.nl>
2017-03-10 15:56:27 +0100

    last fix

--- src/Question.jsx
+++ src/Question.jsx
@@ -5,7 +5,7 @@ class Question extends Component {
 
     constructor(props){
         super(props);
-        //this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     createRadioButtons(){
